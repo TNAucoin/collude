0a821dadb3dfd7c6beb6521f7c8cb4fd456ff081
Travis Aucoin <travisdevaucoin@gmail.com>
2022-11-20 21:35:33 -0600

    renamed django project

--- collude/asgi.py
+++ collude/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for collude_api project.
+ASGI config for collude project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'collude_api.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "collude.settings")
 
 application = get_asgi_application()
